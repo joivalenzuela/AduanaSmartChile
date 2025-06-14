5c350bc02111653f08d854b4e443bf97834d72b6
Jose Valenzuela <joi.valenzuela@duocuc.cl>
2025-06-14 00:49:38 -0400

    estructuracion de repositorio
    se estructura el repositorio en back y front ends, se agrega un microservicio sin empezar en el back y un texto vacio en el front

---
+++ Backend/ServicioDocumentos/src/main/java/com/AduanaSmartChile/ServicioDocumentos/ServicioDocumentosApplication.java
@@ -0,0 +1,13 @@
+package com.AduanaSmartChile.ServicioDocumentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioDocumentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicioDocumentosApplication.class, args);
+	}
+
+}
---
+++ Backend/ServicioDocumentos/src/test/java/com/AduanaSmartChile/ServicioDocumentos/ServicioDocumentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AduanaSmartChile.ServicioDocumentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioDocumentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
